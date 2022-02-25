f022efdf698292ed146cc531bf54206ad757b28f
EremchenkoVeronika <100402499+EremchenkoVeronika@users.noreply.github.com>
2022-02-25 09:40:54 +0300

    hh
    h

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class NumberManipulater
+    {
+        public int FindMin(int a, int b, int z, int x)
+        {
+            int c;
+            c = ((b + z) * (a - b)) / x;
+            return c;
+        }
+
+    }
+
+}
